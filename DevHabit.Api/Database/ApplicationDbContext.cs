3ecf4e75308d1e460ff5e549dacabde427c43181
Barxh <myxyq5@gmail.com>
2026-06-14 20:37:20 +0200

    Configure postgres database

---
+++ DevHabit.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DevHabit.Api.Database.Configurations;
+using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("dev_habit");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HabitConfiguration).Assembly);
+    }
+
+
+}
+
+
--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,3 +1,8 @@
+using DevHabit.Api.Database;
+using DevHabit.Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -6,15 +11,30 @@ using OpenTelemetry.Trace;
 var builder = WebApplication.CreateBuilder(args);
 
 
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+})
+    .AddXmlSerializerFormatters();
 
-builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options
+    .UseNpgsql(
+        builder.Configuration.GetConnectionString("Database"),
+        npgsqlOptionsExtension => npgsqlOptionsExtension
+        .MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemas.Application))
+    .UseSnakeCaseNamingConvention()
+    );
+
 builder.Services.AddOpenTelemetry()
     .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
     .WithTracing(tracing =>
         tracing
         .AddHttpClientInstrumentation()
-        .AddAspNetCoreInstrumentation())
+        .AddAspNetCoreInstrumentation()
+        .AddNpgsql())
     .WithMetrics(metrics => metrics
         .AddHttpClientInstrumentation()
         .AddAspNetCoreInstrumentation()
@@ -31,6 +51,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    await app.ApplyMigrationAsync();
 } 
 
 app.UseHttpsRedirection();
